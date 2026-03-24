fb8c6552b1212b944ef5113e1c34301ff3221da2
Lucien <github.lucien@proton.me>
2026-03-24 10:50:34 +0100

    Task 5: Quiet git commands

--- script.bash
+++ script.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/LaLucien/M324-Exam.git
-cd M324-Exam
-git switch $1
+git clone https://github.com/LaLucien/M324-Exam.git --quiet
+cd M324-Exam 
+git switch $1 --quiet
 echo "This is an AI Review for: " && git diff master | head -c 100
